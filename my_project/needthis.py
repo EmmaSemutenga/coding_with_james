9cd81dd7c3b61fb01ab9cc21635894f303461e59
Emmanuel Semutenga <emzsema@gmail.com>
2021-08-24 19:37:12 +0300

    Added django files

---
+++ my_project/needthis.py
@@ -0,0 +1,7 @@
+What you need to know in python
+variables, names
+datatypes: strings, lists[], dictionaries{ "one":1, "two":2}, tuples(5,6,88,6), sets{8,9,8,6}, int, float
+functions: def
+loops: for, while
+conditional statement: if elif else
+classes:
