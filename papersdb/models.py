467ee4cb868d575b80222207bf7d71e75101a6f1
Emmanuel Semutenga <emzsema@gmail.com>
2021-08-27 18:34:45 +0300

    created app

---
+++ papersdb/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+# Papers Database
+
+# Problem Statement:
+# Read publications but sometimes I want to link the papers / books / essays together through keywords.
+
+# Example
+# Paper 1 - smartphone language learning. << keyword
+
+
+# Author(s)
+# Date of publication
+# Title
+# Categories (paper, book, essay)
+# Link ()
+# Keywords/Tags/ (ed-tech, sci-fi, )
+# Short description / notes
+
+# (1) A database of papers.
+# (2) 
