cfcf875c2a8d1498a1d2dabd1f2a09683ea610ab
james.webb <jamesrobertwebb@gmail.com>
2021-08-31 17:14:43 +0100

    added notes to urls.py

--- papersdb/views.py
+++ papersdb/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    print(request.method)
+    print(request.method) # We want to know what method is being used.
     return render(request, 'index.html', {"name":"James From Brighton"})
+    # render() takes 3 arguments: the original request, the template name, and any other data you want to send to the template in the form or a dictionary.
 
 # GET
 # POST
