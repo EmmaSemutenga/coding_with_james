67156158646d369e14a138a9b6d71ceebc9734c1
james.webb <jamesrobertwebb@gmail.com>
2021-09-16 18:02:30 +0100

    added new html pages

--- papersdb/urls.py
+++ papersdb/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from papersdb.views import home_page, add_paper
+# from papersdb.views import home_page, add_paper, add_author, author_list
+from . import views # . here is the current directory, which is papersdb, and here we import the whole views module.
+
+app_name = 'papersdb' # We add this here because if you end up with e.g. 10 apps in a project, naming paths can get messy and you can unwillingly double
+# up. This is called URL NAME SPACING.
 
 urlpatterns = [
-    path('', home_page),
-    path('my_paper/', add_paper),
+    path('', views.home_page, name='home_page'),
+    path('my_paper/', views.add_paper), # We need to add views. here if you use line 3 above instead of line 2.
+    path('author/', views.add_author), # The first argument here is the address in the url/address bar which links to this. If this is matched the view is called.
+    path('list_of_authors/', views.author_list, name='author_list_page'),
+    path('author_details/<int:id>/', views.author_detail, name='author_detail'),
 ]
--- papersdb/views.py
+++ papersdb/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from my_project import hobbies
-from .models import Paper
+from .models import Paper, Author
 from .forms import PaperForm
 
 # Create your views here.
 def home_page(request):
     papers = Paper.objects.all()
-    return render(request, 'index.html', {"james_papers":papers})
+    return render(request, 'index.html', {"james_papers":papers}) # The keys and the values are kind of the same, but the key is a string and the values are the values.
     # render() takes 3 arguments: the original request, the template name, and any other data you want to send to the template in the form of a dictionary.
 
 def add_paper(request):
@@ -14,3 +14,19 @@ def add_paper(request):
     # print(request.method)
     return render(request, 'add_paper.html', {"james_form":form, "my_request": request.method, })
 
+def add_author(request):
+    if request.method == 'POST': # request is an object and method is one of the attributes it has. The data will be sent in the request object.
+        author = request.POST['author_name'] # The POST object is a dictionary.
+        Author.objects.create(name = author)
+        return redirect('author_list_page') # This name is available to views.py from urls.py. return here can end the function if the if statement is True; if not the return on line 23 ends the function.
+    #   print(request.POST["author_name"]) # print() is a Python function that will print to the terminal. This is often used for testing and debugging.                  
+    return render(request, 'add_author.html') # This is like the else case.
+
+def author_list(request):
+    authors = Author.objects.all()
+    return render(request, 'authors.html', {"james_authors":authors})
+
+def author_detail(request, id):
+    print(id)
+    author = Author.objects.get(id=id) # The id on the left is the column you want to check. The first = here is an assignment in Python; but the second = is an equals in SQL (i.e. what would be == in Py.)
+    return render(request, 'author_detail.html', {"james_author_details":author})
