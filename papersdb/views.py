484d6b778a6aa414c17667f468d5d350e7c07621
james.webb <jamesrobertwebb@gmail.com>
2021-09-26 14:59:27 +0100

    minor changes

--- papersdb/views.py
+++ papersdb/views.py
@@ -28,6 +28,15 @@ def add_author(request):
     # print(request.POST["author_name"]) # print() is a Python function that will print to the terminal. This is often used for testing and debugging.                  
     return render(request, 'add_author.html', {"pineapple":form}) # This is like an else case.
 
+def edit_author(request, id): # Here we receive a request and an id
+    author = Author.objects.get(id=id)    
+    if request.method == 'POST':
+        author_name = request.POST['author_name'] # We are getting the submitted value from the form (activated when the Edit Author button is clicked).
+        author.name = author_name # Here we update the name of the author object
+        author.save()
+        return redirect('papersdb:author_list_page')
+    return render(request, 'edit_author.html', {"james_author_details":author}) # This will only run if the request is (e.g.) 'GET'.
+    
 def author_list(request):
     authors = Author.objects.all()
     return render(request, 'authors.html', {"james_authors":authors})
@@ -37,15 +46,6 @@ def author_detail(request, id):
     print(id)
     author = Author.objects.get(id=id) # The id on the left is the column you want to check. The first = here is an assignment in Python; but the second = is an equals in SQL (i.e. what would be == in Py.)
     return render(request, 'author_detail.html', {"james_author_details":author})
-
-def edit_author(request, id): # Here we receive a request and an id
-    author = Author.objects.get(id=id)    
-    if request.method == 'POST':
-        author_name = request.POST['author_name'] # We are getting the submitted value from the form (activated when the Edit Author button is clicked).
-        author.name = author_name # Here we update the name of the author object
-        author.save()
-        return redirect('papersdb:author_list_page')
-    return render(request, 'edit_author.html', {"james_author_details":author}) # This will only run if the request is (e.g.) 'GET'.
 
 def delete_author(request, id):
     author = Author.objects.get(id=id)
